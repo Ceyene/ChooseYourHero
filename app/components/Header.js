a928d045492e0691b8d5ff1e7e24442bfa329f9b
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-14 18:40:53 -0300

    Removing hamburguer menu and dark theme

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,12 +7,8 @@ export function Header() {
             <img src="app/assets/images/Avengers.png" alt="logo" id="logo" class="logo" />
         </a>
         <nav>
-            <button id="burger" class="burger"><i id="burger_icon" class="fas fa-bars"></i></button>
-            <ul class="menu invisible" id="menu">
-                <li class="menu__item--themes" id="theme_btn"><p>Dark Mode</p></li>
-                <li class="menu__line"></li>
+            <ul class="menu" id="menu">
                 <li class="menu__item" id="fav_link"><a href="#/favorites">My Team</a></li>
-                <li class="menu__line"></li>
             </ul>
         </nav>
     `;
