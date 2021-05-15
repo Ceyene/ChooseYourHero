1432a1dd0d40fc6cd9dd61e2045f5576ff252812
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-15 00:13:51 -0300

    Minor fixes

--- app/helpers/marvel_api.js
+++ app/helpers/marvel_api.js
@@ -1,7 +1,7 @@
 /* -------------------- Api Data ------------------ */
 
 const PUBLIC_KEY = "22a1d52e2fa189d160b90406a5b38847";
-const ENDPOINT = "http://gateway.marvel.com/";
+const ENDPOINT = "https://gateway.marvel.com/";
 const ALL_HEROES = "v1/public/characters";
 const BY_NAME = "v1/public/characters?nameStartsWith=";
 
