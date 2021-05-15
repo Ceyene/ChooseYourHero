92f7fc5cbb50ccb0e9184aa223693d9f8ee213d0
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-15 00:22:29 -0300

    Modifying empty descriptions in hero page

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,12 +2,14 @@ export function Hero(props) {
   const { name, description, thumbnail, urls } = props;
   const url = urls[0].url;
   const img = `${thumbnail.path}.${thumbnail.extension}`;
+  const heroDescription =
+    description === "" ? "Description not available." : description;
   return `
         <article class="hero__page">
             <img class="hero__page--img" src=${img} alt=${name}/>
             <div>
                 <h2 class="section__title" >${name}</h2>
-                <p class="text" >${description}</p>
+                <p class="text" >${heroDescription}</p>
                 <a class="see_more_title" href=${url} >See more at the official web</a>
             </div>
         </article>
